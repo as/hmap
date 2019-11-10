0dc8b678166a9d0df73970132c42391d360d971d
as <as.utf8@gmail.com>
2019-11-10 06:53:25 -0800

    get/put/del

--- map.go
+++ map.go
@@ -6,6 +6,22 @@ type M struct {
 	cap int
 }
 
+func (h *M) Put(k, v string) {
+	h.ensure()
+	h.put(k, v)
+}
+func (h *M) Get(k string) (interface{}, bool) {
+	if len(h.b) == 0 {
+		return nil, false
+	}
+	return h.get(k, false)
+}
+func (h *M) Del(k string) (interface{}, bool) {
+	if len(h.b) == 0 {
+		return nil, false
+	}
+	return h.get(k, true)
+}
 type n struct {
 	k string
 	v string
@@ -14,4 +30,4 @@ type n struct {
 
 func (h *M) put(k, v string) {}
 func (h *M) get(k string, del bool) (interface{}, bool) {}
-func (h *M) ensure() {}}
+func (h *M) ensure() {}
